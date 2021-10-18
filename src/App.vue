89caaa8635a4a1347883eb048ba8c8eb7abecdcf
Si HaMaDa <nrookman_27@yahoo.com>
2021-10-18 03:33:26 +0200

    add MyHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link>
-    </div>
+    <MyHeader />
     <router-view />
   </div>
 </template>
+<script>
+import MyHeader from "@/components/MyHeader";
+
+export default {
+  name: "App",
+  components: {
+    MyHeader,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="header">
+    <div class="logo">Vue Train</div>
+    <ul id="nav">
+      <router-link tag="li" to="/" exact>Home</router-link>
+      |
+      <router-link tag="li" to="/about">About</router-link>
+      |
+      <router-link tag="li" to="/contact">Contact</router-link>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#header {
+  background-color: #ddd;
+  color: green;
+  .logo {
+    float: left;
+    padding: 25px;
+    font-size: 25px;
+  }
+  ul li {
+    display: inline-block;
+  }
+}
+</style>
